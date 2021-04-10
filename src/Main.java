cb475e53f98c808677d1d0597691c3454099accf
Salo1802 <salo1802@hotmail.com>
2021-04-09 21:08:05 -0500

    cree el codigo en eclipse

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+	
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+	
+	@Override
+	public void settings() {
+		size(400,400);
+		 
+		 
+	}
+	
+	
+
+	
+	
+	@Override
+	public void setup() {
+	
+		  
+	}
+	
+@Override
+public void draw() {
+	
+	
+	  
+     
+     
+
+     
+    
+	}
+
+ }
+
